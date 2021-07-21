6e643454c9777bb85b73a9ec03155eef351e9443
hikerpig <hikerpigwinnie@gmail.com>
2021-07-21 13:50:17 +0800

    remove hostname in demo and docs url

--- demo/src/const.ts
+++ demo/src/const.ts
@@ -2,4 +2,4 @@ export const DEMO_BASE_URL = '/demo/'
 
 export const GITHUB_URL = 'https://github.com/hikerpig/pintora'
 
-export const DOC_URL = 'https://pintorajs.netlify.app'
+export const DOC_URL = '/'
