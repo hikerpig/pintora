53d8c4b5e573242c0de80d921ea4e6ade52ae454
hikerpig <hikerpigwinnie@gmail.com>
2021-12-25 18:20:04 +0800

    chore: upgrade dependencies and run build-site with turbo

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,6 +1,9 @@
 # npm run build --workspace=demo
 # npm run build --workspace=website
 
-pushd demo && npm run build && popd
-pushd website && npm run build && popd
+# pushd demo && npm run build && popd
+# pushd website && npm run build && popd
+# cp -r demo/dist/ website/build/demo
+
+npx turbo run build
 cp -r demo/dist/ website/build/demo
