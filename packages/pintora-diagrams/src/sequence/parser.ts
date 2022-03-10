3c4b25a6ce5ce6f361843d237a439d1256eea295
hikerpig <hikerpigwinnie@gmail.com>
2022-03-10 18:19:24 +0800

    optimize(pintora-diagrams): improve parser performance by eliminating some ambiguity

--- packages/pintora-diagrams/src/sequence/parser.ts
+++ packages/pintora-diagrams/src/sequence/parser.ts
@@ -5,14 +5,9 @@ import { genParserWithRules } from '../util/parser-util'
 setYY(db)
 
 export const parse = genParserWithRules(grammar, {
+  dedupeAmbigousResults: true,
   postProcess(results) {
-    let validResults = results
-    // another hack to avoid duplicate results,
-    // @FIXME: need to fix activityDiagram.ne grammar and remove this hack
-    if (Array.isArray(results[0])) {
-      validResults = results[0]
-    }
-    db.apply(validResults as any)
-    return validResults
+    db.apply(results as any)
+    return results
   },
 })
