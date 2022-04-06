f4ce03552f171dca4c70e956232c58c78b4b749b
hikerpig <hikerpigwinnie@gmail.com>
2022-04-06 19:21:13 +0800

    fix(demo): service worker error (#28)

--- demo/src/sw.ts
+++ demo/src/sw.ts
@@ -2,6 +2,8 @@
 import { precacheAndRoute } from 'workbox-precaching'
 import { clientsClaim, skipWaiting } from 'workbox-core'
 
+declare let self: ServiceWorkerGlobalScope
+
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
 
