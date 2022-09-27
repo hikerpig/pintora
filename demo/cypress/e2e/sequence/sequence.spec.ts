021988a584c19794fa09058ad10ef6597ff33c89
hikerpig <hikerpigwinnie@gmail.com>
2022-09-27 15:49:31 +0800

    fix: [sequenceDiagram] excessive box width bug (#154)

--- demo/cypress/e2e/sequence/sequence.spec.ts
+++ demo/cypress/e2e/sequence/sequence.spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="@percy/cypress" />
 import { EXAMPLES } from '@pintora/test-shared'
-import { startRender } from '../test-utils/render'
+import { makeSnapshotCases, startRender } from '../test-utils/render'
 
 describe('Sequence Diagram', () => {
   it('renders', () => {
@@ -9,4 +9,21 @@ describe('Sequence Diagram', () => {
 
     cy.percySnapshot()
   })
+
+  makeSnapshotCases([
+    {
+      description: 'excessive box width bug, #153',
+      code: `
+      sequenceDiagram
+      participant service_2 as "Service 2"
+      participant service_3 as "Service 3"
+      participant service_4 as "Service 4"
+
+      par one
+        service_2->>service_3: random
+        service_4->>service_4: problem
+      end
+      `,
+    },
+  ])
 })
