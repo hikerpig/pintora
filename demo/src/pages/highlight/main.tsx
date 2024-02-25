bac2ed2c312c6d9a4fe2cd04a5315bcc1e90bc44
hikerpig <hikerpigwinnie@gmail.com>
2024-02-25 20:13:50 +0800

    fix: adapt to shiki v1

--- demo/src/pages/highlight/main.tsx
+++ demo/src/pages/highlight/main.tsx
@@ -1,102 +1,92 @@
-// import * as vsctm from 'vscode-textmate'
-import * as shiki from 'shiki'
+// // import * as vsctm from 'vscode-textmate'
+// import * as shiki from 'shiki'
 
-const TEST_TEXT = `
-mindmap
-* Root
-** Second
-    `
+// const TEST_TEXT = `
+// mindmap
+// * Root
+// ** Second
+//     `
 
-async function fetchWasm() {
-  const res = await fetch('/data/onig.wasm')
-  return await res.arrayBuffer()
-}
+// // async function start() {
+// //   const wasmBin = await fetchWasm()
+// //   const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
+// //     return {
+// //       createOnigScanner(patterns) {
+// //         return new oniguruma.OnigScanner(patterns)
+// //       },
+// //       createOnigString(s) {
+// //         return new oniguruma.OnigString(s)
+// //       },
+// //     }
+// //   })
+// //   const registry = new vsctm.Registry({
+// //     onigLib: vscodeOnigurumaLib,
+// //     loadGrammar: async scopeName => {
+// //       console.log('loadGrammar', scopeName)
+// //       let grammar = null
 
-// async function start() {
-//   const wasmBin = await fetchWasm()
-//   const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
-//     return {
-//       createOnigScanner(patterns) {
-//         return new oniguruma.OnigScanner(patterns)
-//       },
-//       createOnigString(s) {
-//         return new oniguruma.OnigString(s)
-//       },
-//     }
+// //       if (/pintora/.test(scopeName)) {
+// //         const syntax = await (await fetch('/data/pintora.tmLanguage.json')).text()
+// //         grammar = vsctm.parseRawGrammar(syntax, 'pintora.tmLanguage.json')
+// //       }
+// //       return grammar
+// //     },
+// //   })
+
+// //   // console.log('registry', registry)
+
+// //   registry.loadGrammar('source.pintora').then(grammar => {
+// //     const lines = TEST_TEXT.split('\n')
+
+// //     let ruleStack = vsctm.INITIAL
+// //     for (let i = 0; i < lines.length; i++) {
+// //       const line = lines[i]
+// //       const lineTokens = grammar?.tokenizeLine(line, ruleStack)
+// //       // console.log('tokens', lineTokens)
+
+// //       if (lineTokens) {
+// //         for (let j = 0; j < lineTokens.tokens.length; j++) {
+// //           const token = lineTokens.tokens[j]
+// //           console.log(
+// //             ` - token from ${token.startIndex} to ${token.endIndex} ` +
+// //               `(${line.substring(token.startIndex, token.endIndex)}) ` +
+// //               `with scopes ${token.scopes.join(', ')}`,
+// //           )
+// //         }
+
+// //         ruleStack = lineTokens.ruleStack
+// //       }
+// //     }
+// //   })
+
+// //   return registry
+// // }
+
+// async function startShiki() {
+//   const syntaxJson = await (await fetch('/data/pintora.tmLanguage.json')).json()
+
+//   const pintoraLanguage: any = {
+//     id: 'pintora',
+//     scopeName: 'source.pintora',
+//     grammar: syntaxJson,
+//     path: '',
+//   }
+
+//   const highlighter = await shiki.getHighlighter({
+//     langs: [pintoraLanguage, 'json'],
+//     themes: ['material-theme-palenight', 'dracula'],
 //   })
-//   const registry = new vsctm.Registry({
-//     onigLib: vscodeOnigurumaLib,
-//     loadGrammar: async scopeName => {
-//       console.log('loadGrammar', scopeName)
-//       let grammar = null
+//   await highlighter.loadLanguage(pintoraLanguage)
 
-//       if (/pintora/.test(scopeName)) {
-//         const syntax = await (await fetch('/data/pintora.tmLanguage.json')).text()
-//         grammar = vsctm.parseRawGrammar(syntax, 'pintora.tmLanguage.json')
-//       }
-//       return grammar
-//     },
-//   })
-
-//   // console.log('registry', registry)
-
-//   registry.loadGrammar('source.pintora').then(grammar => {
-//     const lines = TEST_TEXT.split('\n')
-
-//     let ruleStack = vsctm.INITIAL
-//     for (let i = 0; i < lines.length; i++) {
-//       const line = lines[i]
-//       const lineTokens = grammar?.tokenizeLine(line, ruleStack)
-//       // console.log('tokens', lineTokens)
-
-//       if (lineTokens) {
-//         for (let j = 0; j < lineTokens.tokens.length; j++) {
-//           const token = lineTokens.tokens[j]
-//           console.log(
-//             ` - token from ${token.startIndex} to ${token.endIndex} ` +
-//               `(${line.substring(token.startIndex, token.endIndex)}) ` +
-//               `with scopes ${token.scopes.join(', ')}`,
-//           )
-//         }
-
-//         ruleStack = lineTokens.ruleStack
-//       }
-//     }
-//   })
-
-//   return registry
+//   return { highlighter }
 // }
 
-async function startShiki() {
-  const wasmBin = await fetchWasm()
-  const syntaxJson = await (await fetch('/data/pintora.tmLanguage.json')).json()
+// // start()
 
-  const pintoraLanguage = {
-    id: 'pintora',
-    scopeName: 'source.pintora',
-    grammar: syntaxJson,
-    path: '',
-  }
-
-  shiki.setCDN('https://unpkg.com/shiki/')
-  shiki.setWasm(wasmBin)
-
-  const highlighter = await shiki.getHighlighter({
-    langs: [pintoraLanguage, 'json'],
-    themes: ['material-palenight', 'dracula'],
-    theme: 'material-palenight',
-  })
-  await highlighter.loadLanguage(pintoraLanguage)
-
-  return { highlighter }
-}
-
-// start()
-
-startShiki().then(({ highlighter }) => {
-  const html = highlighter.codeToHtml(TEST_TEXT, { lang: 'pintora' })
-  const resultDiv = document.createElement('div')
-  resultDiv.id = 'result'
-  resultDiv.innerHTML = html
-  document.body.appendChild(resultDiv)
-})
+// startShiki().then(({ highlighter }) => {
+//   const html = highlighter.codeToHtml(TEST_TEXT, { lang: 'pintora' })
+//   const resultDiv = document.createElement('div')
+//   resultDiv.id = 'result'
+//   resultDiv.innerHTML = html
+//   document.body.appendChild(resultDiv)
+// })
--- website/plugins/docusaurus-theme-pintora/theme/PintoraPlay/highlight.ts
+++ website/plugins/docusaurus-theme-pintora/theme/PintoraPlay/highlight.ts
@@ -3,49 +3,27 @@ import * as shiki from 'shiki'
 export const shikiLightTheme = 'material-theme'
 export const shikiDarkTheme = 'dracula'
 
-let fetchWasmPromise: Promise<ArrayBuffer> | null = null
-let wasmData: ArrayBuffer
-async function fetchWasm() {
-  // const res = await fetch('/data/onig.wasm')
-  if (wasmData) return wasmData
-
-  if (!fetchWasmPromise) {
-    fetchWasmPromise = fetch('https://unpkg.com/vscode-oniguruma@1.7.0/release/onig.wasm')
-      .then(res => res.arrayBuffer())
-      .then(data => {
-        wasmData = data
-        return wasmData
-      })
-  }
-  return await fetchWasmPromise
-}
-
 let globalHighlighter: shiki.Highlighter
 let globalHighlighterPromise: Promise<shiki.Highlighter>
 
 export async function startShiki(opts: { isDarkMode?: boolean } = {}) {
   if (!globalHighlighter) {
-    const wasmBin = await fetchWasm()
-    const syntaxJson = await (await fetch('/data/pintora.tmLanguage.json')).json()
+    const syntaxJson: shiki.RawGrammar = await (await fetch('/data/pintora.tmLanguage.json')).json()
 
-    const pintoraLanguage = {
-      id: 'pintora',
+    const pintoraLanguage: shiki.LanguageRegistration = {
+      name: 'pintora',
       scopeName: 'source.pintora',
-      grammar: syntaxJson,
-      path: '',
+      // path: '',
+      embeddedLangsLazy: ['json'],
+      ...syntaxJson,
     }
-
-    shiki.setCDN('https://unpkg.com/shiki/')
-    // shiki.setCDN('https://cdn.skypack.dev/shiki/')
-    shiki.setWasm(wasmBin)
 
     const theme = opts.isDarkMode ? shikiDarkTheme : shikiLightTheme
 
     if (!globalHighlighterPromise) {
       globalHighlighterPromise = shiki.getHighlighter({
         langs: [pintoraLanguage, 'json'],
-        themes: [shikiLightTheme, shikiDarkTheme],
-        theme,
+        themes: [theme],
       })
     }
 
