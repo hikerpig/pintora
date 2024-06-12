10da9e6e842ef584c69341ea38e8b86850897a2f
hikerpig <hikerpigwinnie@gmail.com>
2024-06-12 21:54:31 +0800

    chore(dep): upgrade eslint to v9 and fix fatal errors

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+//@ts-check
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import unusedImportPlugin from 'eslint-plugin-unused-imports'
+
+export default [
+  ...tseslint.config(...tseslint.configs.recommended),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      'unused-imports': unusedImportPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-this-alias': 'warn',
+      '@typescript-eslint/no-extra-semi': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
+      'unused-imports/no-unused-imports': 'error',
+      'prefer-spread': 0,
+      'prettier/prettier': 'warn',
+    },
+  },
+]
