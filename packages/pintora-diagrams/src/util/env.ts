a70a6a58a3b3bba95a30ef219707be7c1004d7f8
flowkclav <57037551+flowkclav@users.noreply.github.com>
2025-05-07 09:51:07 -0400

    fix (diagrams) Adding more sepcific condition to determine the value of `isDev` (#371)

--- packages/pintora-diagrams/src/util/env.ts
+++ packages/pintora-diagrams/src/util/env.ts
@@ -1,1 +1,1 @@
-export const isDev = typeof location !== 'undefined' && location.hostname === 'localhost'
+export const isDev = typeof location !== 'undefined' && location.hostname === 'localhost' && typeof window !== 'undefined'
